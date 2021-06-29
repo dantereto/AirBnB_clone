74d03e377982ff81697a39493eafa72a0ba42462
dantereto <2247@holbertonschool.com>
2021-06-29 08:07:08 -0500

    test base

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,6 +4,7 @@ import uuid
 from models.base_model import BaseModel
 from datetime import datetime, date
 
+
 class TestBaseModel(unittest.TestCase):
 
     def test_cases(self):
@@ -11,6 +12,7 @@ class TestBaseModel(unittest.TestCase):
         ids = uuid.UUID(my_model.id)
         self.assertIsInstance(ids, uuid.UUID)
         self.assertIsInstance(my_model.created_at, datetime)
-        
+
+
 if __name__ == '__main__':
     unittest.main()
