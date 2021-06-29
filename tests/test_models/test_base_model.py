b09acd5106d2b9e8049b43680767706517741d9f
dantereto <2247@holbertonschool.com>
2021-06-29 08:21:30 -0500

    test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import pep8
 import unittest
 import uuid
 from models.base_model import BaseModel
@@ -12,7 +13,8 @@ class TestBaseModel(unittest.TestCase):
         ids = uuid.UUID(my_model.id)
         self.assertIsInstance(ids, uuid.UUID)
         self.assertIsInstance(my_model.created_at, datetime)
-
-
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0, "Check pep8")
 if __name__ == '__main__':
     unittest.main()
