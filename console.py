1bce2ec08edd4c5f1df6a16da7878e295ed55651
dantereto <2247@holbertonschool.com>
2021-06-29 09:49:46 -0500

    console

--- console.py
+++ console.py
@@ -1,33 +1,122 @@
 #!/usr/bin/python3
-
-"""Module to storage a file"""
-
-from models.base_model import BaseModel
+"""Module to storage a file
+"""
 from models.user import User
+import cmd
+import sys
+from shlex import split
+from models.base_model import BaseModel
+from models import storage
 from models.city import City
 from models.amenity import Amenity
+from models.state import State
 from models.place import Place
 from models.review import Review
-from models.state import State
-from models import storage
-import cmd
+objects = {'User': User, 'BaseModel': BaseModel, 'Place': Place,
+           'State': State, 'City': City, 'Amenity': Amenity,
+           'Review': Review}
 
 
 class HBNBCommand(cmd.Cmd):
-    """funcion principal prueba"""
+    """ console """
+
+
     prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """quit the program"""
+        'Quit command to exit the program'
+        exit()
+
+    def do_EOF(self, arg):
+        """Quit the program"""
+        'EOF command to exit the program'
+        exit()
 
     def emptyline(self):
         """pass when found a empty line"""
         pass
 
-    def do_quit(self, arg):
-        """Quit command to exit the program"""
-        return True
+    def do_create(self, arg):
+        """create a file"""
+        'create a json file'
+        arg = split(arg)
+        if arg == []:
+            print('** class name missing **')
+        elif arg[0] not in objects.keys():
+            print("** class doesn't exist **")
+        else:
+            storage.reload()
+            my_model = objects[arg[0]]()
+            storage.new(my_model)
+            my_model.save()
+            print(my_model.id)
 
-    def do_EOF(self, arg):
-        """EOF command to exit the program"""
-        return True
+    def do_show(self, arg):
+        """show the file"""
+        arg = split(arg)
+        if arg == []:
+            print('** class name missing **')
+        elif arg[0] not in objects.keys():
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print('** instance id missing **')
+        else:
+            for key, value in storage.all().items():
+                if value.id == arg[1] and value.__class__.__name__ == arg[0]:
+                    print(str(value))
+                    return
+            print('** no instance found **')
+
+    def do_destroy(self, arg):
+        """destroy the file"""
+        arg = split(arg)
+        if arg == []:
+            print('** class name missing **')
+        elif arg[0] not in objects.keys():
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print('** instance id missing **')
+        else:
+            for key, value in storage.all().items():
+                if value.id == arg[1] and value.__class__.__name__ == arg[0]:
+                    storage.all().pop(key)
+                    return
+            print('** no instance found **')
+
+    def do_all(self, arg):
+        """show all the files"""
+        arg = split(arg)
+        if arg == []:
+            lista = []
+            for key, value in storage.all().items():
+                lista.append(value.__str__())
+            print(lista)
+        elif arg[0] not in objects.keys():
+            print("** class doesn't exist **")
+        else:
+            lista = []
+            for key, value in storage.all().items():
+                if arg[0] == value.__class__.__name__:
+                    lista.append(value.__str__())
+            print(lista)
+
+    def do_update(self, arg):
+        """update a new element to the file"""
+        arg = split(arg)
+        if arg == []:
+            print('** class name missing **')
+        elif arg[0] not in objects.keys():
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print('** instance id missing **')
+        elif len(arg) == 2:
+            print('** attribute name missing **')
+        elif len(arg) == 3:
+            print('** value missing **')
+        else:
+            for key, value in storage.all().items():
+                setattr(value, arg[2], arg[3])
 
 
 if __name__ == '__main__':
