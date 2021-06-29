e3c1274e6ceb4ff415b3c478361f06a3d0ca209b
dantereto <2247@holbertonschool.com>
2021-06-29 09:36:21 -0500

    try

--- console.py
+++ console.py
@@ -4,7 +4,6 @@
 
 
 import cmd
-import shlex
 from models.base_model import BaseModel
 from models.user import User
 from models.city import City
@@ -12,7 +11,6 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 from models.state import State
-from models import storage
 
 
 objects = {'User': User, 'BaseModel': BaseModel, 'Place': Place,
@@ -35,86 +33,6 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, arg):
         """EOF command to exit the program"""
         return True
-
-    def do_create(self, arg):
-        """create a json file"""
-        arg = shlex.split(arg)
-        if arg == []:
-            print('** class name missing **')
-        elif arg[0] not in objects.keys():
-            print("** class doesn't exist **")
-        else:
-            storage.reload()
-            my_model = objects[arg[0]]()
-            storage.new(my_model)
-            my_model.save()
-            print(my_model.id)
-
-    def do_show(self, arg):
-        """show the file"""
-        arg = shlex.split(arg)
-        if arg == []:
-            print('** class name missing **')
-        elif arg[0] not in objects.keys():
-            print("** class doesn't exist **")
-        elif len(arg) == 1:
-            print('** instance id missing **')
-        else:
-            for key, value in storage.all().items():
-                if value.id == arg[1] and value.__class__.__name__ == arg[0]:
-                    print(str(value))
-                    return
-            print('** no instance found **')
-
-    def do_destroy(self, arg):
-        """destroy the file"""
-        arg = shlex.split(arg)
-        if arg == []:
-            print('** class name missing **')
-        elif arg[0] not in objects.keys():
-            print("** class doesn't exist **")
-        elif len(arg) == 1:
-            print('** instance id missing **')
-        else:
-            for key, value in storage.all().items():
-                if value.id == arg[1] and value.__class__.__name__ == arg[0]:
-                    storage.all().pop(key)
-                    return
-            print('** no instance found **')
-
-    def do_all(self, arg):
-        """show all the files"""
-        arg = shlex.split(arg)
-        if arg == []:
-            lista = []
-            for key, value in storage.all().items():
-                lista.append(value.__str__())
-            print(lista)
-        elif arg[0] not in objects.keys():
-            print("** class doesn't exist **")
-        else:
-            lista = []
-            for key, value in storage.all().items():
-                if arg[0] == value.__class__.__name__:
-                    lista.append(value.__str__())
-            print(lista)
-
-    def do_update(self, arg):
-        """update a new element to the file"""
-        arg = shlex.split(arg)
-        if arg == []:
-            print('** class name missing **')
-        elif arg[0] not in objects.keys():
-            print("** class doesn't exist **")
-        elif len(arg) == 1:
-            print('** instance id missing **')
-        elif len(arg) == 2:
-            print('** attribute name missing **')
-        elif len(arg) == 3:
-            print('** value missing **')
-        else:
-            for key, value in storage.all().items():
-                setattr(value, arg[2], arg[3])
 
 
 if __name__ == '__main__':
