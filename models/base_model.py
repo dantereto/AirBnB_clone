b3edfb126a7412207d31af2526687626d98c9766
dantereto <2247@holbertonschool.com>
2021-06-29 12:06:36 -0500

    base

--- models/base_model.py
+++ models/base_model.py
@@ -23,6 +23,8 @@ class BaseModel:
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 if key != '__class__':
                     setattr(self, key, value)
+                else:
+                    pass
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -40,7 +42,6 @@ class BaseModel:
         """Update the current time"""
 
         self.updated_at = datetime.now()
-        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
