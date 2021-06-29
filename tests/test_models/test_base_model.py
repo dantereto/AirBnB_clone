d6af08e1f40587b2a367ab3f54f782a966acfec9
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-06-29 15:34:39 -0500

    Documentation

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+"""Module to Tes
+"""
 import pep8
 import unittest
 import uuid
@@ -8,8 +9,12 @@ from datetime import datetime, date
 
 
 class TestBaseModel(unittest.TestCase):
-
+    """TestBaseModel class
+    """
     def test_cases(self):
+        """test the principals cases
+        """
+
         my_model = BaseModel()
         ids = uuid.UUID(my_model.id)
         self.assertIsInstance(ids, uuid.UUID)
@@ -17,21 +22,27 @@ class TestBaseModel(unittest.TestCase):
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['models/base_model.py'])
         self.assertEqual(result.total_errors, 0, "Check pep8")
+
     def test_create_base_model(self):
+        """Test when a base_model is created
+        """
+
         my_model = BaseModel()
         self.assertEqual(type(my_model.id), str)
         self.assertEqual(my_model.created_at, my_model.updated_at)
 
     def test_save(self):
+        """Test when save the model
+        """
+
         my_model = BaseModel()
         my_model.save()
         self.assertNotEqual(my_model.created_at, my_model.updated_at)
 
     def test_to_dict(self):
+        """Test when add the new attributes to the list
+        """
+
         my_model = BaseModel()
         result = my_model.to_dict()
         self.assertEqual(result['__class__'], 'BaseModel')
-
-
-if __name__ == '__main__':
-    unittest.main()
