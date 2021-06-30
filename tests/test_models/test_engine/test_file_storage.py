5bfd25f8dec0b529fd845d5f1954c16fb42e7890
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-06-30 16:34:21 -0500

    Add init

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
 """Module to test file storage
 """
-import unittest
-from models import storage
-from models.base_model import BaseModel
 
 
-class TestFileStorage(unittest.TestCase):
+class TestFileStorage(unittest, TestCase):
     """TestsFileStorage Class
     """
 
-    def test_all(self):
-        """Test when return objects
-        """
-
-        my_model = BaseModel()
-        storage.new(my_model)
-        self.assertEqual(type(storage.all()), dict)
+    pass
